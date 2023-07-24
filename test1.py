9806e776a13c46fd1a6629d090fe2814a6b357f6
samruddhi ubhe <138124231+SamruddhiUbhe28@users.noreply.github.com>
2023-07-24 17:29:00 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,5 @@
 num1 = int(input("enter the number"))
 num2 = int(input("enter the number"))
-def add(num1,num2):
-  sum = 0
-  sum = num1 + num2
-  return sum
-print("Addition is:", add(num1,num2))
+sum = num1 + num2
+print(sum)
+
